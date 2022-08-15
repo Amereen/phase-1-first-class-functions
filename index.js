d1f3b29cba3ad80e2bd2afdea93df24b54c8c1e7
Amereen Hanvel <amereenhanvel786@gmail.com>
2022-08-15 13:47:52 -0400

    commit tree dda763222bfd723d0b76b6cdd773a5ca204c2fce

---
+++ index.js
@@ -0,0 +1,20 @@
+ function receivesAFunction(Spy){
+ return Spy()
+}
+// receivesAFunction(function(){"Spy"})
+
+
+
+  function returnsANamedFunction(){
+    return function name(){
+
+    }
+}
+
+const returnsAnAnonymousFunction= function() {
+  return function () {
+
+  }
+
+  
+}
